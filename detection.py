2f46e9891cdbce801bb98904d8c6825cdb33bb92
Ansan Rai <anshanpool@gmail.com>
2024-01-08 21:56:36 +0545

    added support for model hosted in server

--- detection.py
+++ detection.py
@@ -2,15 +2,9 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import requests
 import json
-
-import asyncio
-
-import tensorflow as tf
 import numpy as np
-from PIL import Image
-import cv2
+import tensorflow as tf
 from cv2 import imshow
-from sort import *
 
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
@@ -20,10 +14,10 @@ class detection:
 
     def __init__(self):
 
-        self.url = 'http://localhost:8501/v1/models/xada_gai:predict'
-        # self.url = 'http://192.168.226.177:8501/v1/models/xada_gai:predict'
+        # self.url = 'http://localhost:8501/v1/models/xada_gai:predict'
 
-        self.mot_tracker = Sort()
+        self.model = tf.saved_model.load('saved_model/')
+
 
         PATH_TO_LABELS = 'saved_model/1/label_map.pbtxt'
         self.category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
@@ -31,11 +25,45 @@ class detection:
 
     def run_inference_for_single_frame(self, frame):
 
-        json_response = requests.post(self.url, data=frame)
-        response = json.loads(json_response.text)
+        ## if you have hosted your model in another server, you can use below code
 
-        output_dict = response['predictions'][0]
-        output_dict['detection_classes'] = np.array(output_dict['detection_classes'], dtype=np.int64)
+        # json_response = requests.post(self.url, data=frame)
+        # response = json.loads(json_response.text)
+        # output_dict = response['predictions'][0]
+        # output_dict['detection_classes'] = np.array(output_dict['detection_classes'], dtype=np.int64)
+
+
+
+        ## if you have hosted your model in your local machine, you can use below code
+        image = np.asarray(frame)
+        #input needs to be a tensor, so converting it
+        input_tensor = tf.convert_to_tensor(image)
+        #model expects input to be a batch, so adding an axis
+        input_tensor = input_tensor[tf.newaxis,...]
+
+        # Run inference
+        model_fn = self.model.signatures['serving_default']
+        output_dict = model_fn(input_tensor)
+
+        # All outputs are batches tensors.
+        # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+        # We're only interested in the first num_detections.
+        num_detections = int(output_dict.pop('num_detections'))
+        output_dict = {key:value[0, :num_detections].numpy()
+                        for key,value in output_dict.items()}
+        output_dict['num_detections'] = num_detections
+
+        # detection classes should be ints.
+        output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
+        # Handle models with masks:
+        if 'detection_masks' in output_dict:
+            # Reframe the the bbox mask to the image size.
+            detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                    output_dict['detection_masks'], output_dict['detection_boxes'],
+                    image.shape[0], image.shape[1])
+            detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
+                                            tf.uint8)
+            output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
 
         return output_dict
 
@@ -53,58 +81,19 @@ class detection:
                 detection_scores.append(output_dict['detection_scores'][i])
                 detection_boxes.append(output_dict['detection_boxes'][i])
         
-        print(detection_classes)
-        self.sendMessage(detection_classes)
+        ##sends the message using SMS API
+        # self.sendMessage(detection_classes)
         return detection_boxes, detection_scores, detection_classes
         
     
     def sendMessage(self, classes):
-        for i in range(len(classes)):
-            if classes[i] == 1:
-                print("cow")
-            elif classes[i] == 2:
-                print("dog")
-            elif classes[i] == 3:
-                print("tiger")
-            elif classes[i] == 4:
-                print("elephant")
+        ## you can add your logic to send SMS message here
+        pass
 
-                
-    
-    def track(self, boxes):
-        
-        boxes = np.array(boxes, dtype=float)
-        
-        track_bbs = self.mot_tracker.update(boxes)
-        new_id = []
-        id = track_bbs[:,4]
-        
-        for i in range(len(id)):
-            new_id.append(int(id[i]))
-        
-        new_id = np.array(new_id)
-        return new_id
     
     def visual(self, frame, boxes, classes, scores, new_id=None):
 
         boxes = np.array(boxes, dtype=float)
-        # new_id = np.array(new_id, dtype=int)
-        # new_id = None
-        # if new_id.shape[0] == boxes.shape[0]:
-        #     vis_util.visualize_boxes_and_labels_on_image_array(
-        #         frame,
-        #         boxes,
-        #         classes,
-        #         scores,
-        #         self.category_index,
-        #         instance_masks=None,
-        #         use_normalized_coordinates=True,
-        #         line_thickness=4,
-        #         # track_ids=new_id
-        #     )
-        #     imshow("detections", frame)
-
-        # else:
         vis_util.visualize_boxes_and_labels_on_image_array(
                 frame,
                 boxes,
--- main.py
+++ main.py
@@ -5,20 +5,8 @@ import time
 
 detector = detection()
 
-# async def sendData(frame):
-#     image_w, image_h, _ = frame.shape
-#     image = frame.reshape(1, image_w, image_h, 3)
-#     data = json.dumps({
-#         "signature_name": "serving_default",
-#         "instances": image.tolist()
-#     })
-#     await detector.predict(data)
-
-
-video_path = "cow.mp4"
+video_path = "[ Path to video you want to detect]"
 video = cv2.VideoCapture(video_path)
-
-# video = cv2.VideoCapture("rtsp://anshan:A_shan11@192.168.225.50:554/Streaming/channels/301")
 
 while video.isOpened():
 
@@ -28,27 +16,20 @@ while video.isOpened():
 
         image_w, image_h, _ = frame.shape
         image = frame.reshape(1, image_w, image_h, 3)
-        data = json.dumps({
-            "signature_name": "serving_default",
-            "instances": image.tolist()
-        })
 
-        # sendData(frame)
-        # asyncio.run(detector.predict(data))
-        boxes, scores, classe = detector.predict(data)
-        # print(type(classe[0]))
-        # new_id = detector.track(boxes)
-        # detector.visual(frame, boxes, classe, scores, new_id)
-        # detector.visual(frame, boxes, classe, scores)
-        # cv2.imshow('frame', frame)
+        ## below code is required if your model is hosted in another server
+        ## it does preprocessing for you before sending it throung http request 
+        
+        # image = json.dumps({
+        #     "signature_name": "serving_default",
+        #     "instances": image.tolist()
+        # })
+
+        boxes, scores, classe = detector.predict(image)
+        detector.visual(frame, boxes, classe, scores)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    else:
-        break
 
-
-# time.sleep(10)
-# When everything done, release the capture
 video.release()
 cv2.destroyAllWindows()
